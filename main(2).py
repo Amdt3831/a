3c19f1a4dea186b604c7d675a1312d8dbddd8440
amir <amirmahmood3831@gmail.com>
2024-01-27 23:16:01 +0330

    4

--- main(2).py
+++ main(2).py
@@ -1,21 +1,45 @@
-import re
-input = input()
-a = input.split(' ')
-b = []
-c = []
-for x in a:
-    b.append(x[0])
-    c.append(x[1:])
+class RoseDictionary(object):
+    def __init__(self):
+        self.t = {}
 
-for u in range(len(c)):
-    c[u] = int(c[u])
-r = zip(b, c)
-t = list(r)
-t.sort(key= lambda x: x[1])
-for u in range(len(t)):
-    t[u] = list(t[u])
-x = []
-for g in t:
-    x.append(g[0])
-u = ''.join(x)
-print(u)
+    def __setitem__(self, key, value):
+        self.t[key] = value
+
+    def __getitem__(self, item):
+        return self.t[item]
+
+    def pop_item(self, raise_error=False, default=None,
+                 error_msg=None):
+        if len(list(self.t)) != 0:
+            a = self.t[list(self.t)[-1]]
+            del self.t[list(self.t)[-1]]
+            return a
+        elif raise_error is False and default is not None:
+            return default
+        elif raise_error is False and default is None:
+            if error_msg is not None:
+                return print(error_msg)
+            else:
+                return print('Dictionary was empty and no default value/message was specified.')
+        elif raise_error is True and error_msg is not None:
+            raise KeyError(error_msg)
+        elif raise_error is True and error_msg is None:
+            return 'Dictionary was empty and no default value/message was specified.'
+
+    def get_item(self,key, raise_error=False, default=None,
+                 error_msg=None):
+        if key in self.t:
+            return self.t[key]
+        elif raise_error is False and default is not None:
+            return default
+        elif raise_error is False and default is None:
+            if error_msg is not None:
+                return print(error_msg)
+            else:
+                return print('Value was not found and no default value/message was specified.')
+        elif raise_error is True and error_msg is not None:
+            raise KeyError(error_msg)
+        elif raise_error is True and error_msg is None:
+            return 'Value was not found and no default value/message was specified.'
+
+
--- main.py
+++ main.py
@@ -1,26 +1,43 @@
-import re
+import numpy as np
+f = open('input1.txt', 'r')
+inp = [line.strip().split(' ') for line in f.readlines()]
+for i in range(1, len(inp)):
+    for j in range(int(inp[0][1])):
+        inp[i][j] = int(inp[i][j])
+inp[0][0] = int(inp[0][0])
+inp[0][1] = int(inp[0][1])
+sets = {}
+y = 1
+for i in range(1, inp[0][0]+1):
+    sets[f'sett{i}'] = list(np.array([]))
+    u = 0
+    while u < inp[0][1]:
+        sets[f'sett{i}'].append(inp[y])
+        y += 1
+        u += 1
+p = 0
+item3 = 0
+item4 = 0
+for item1 in range(1, len(sets) + 1):
+    for item2 in range(item1 + 1, len(sets) + 1):
+        a = np.array(sets[f'sett{item1}'])
+        b = np.array(sets[f'sett{item2}'])
+        c = np.dot(a, b)
+        if np.linalg.det(c) > p:
+            p = np.linalg.det(c)
+            item3 = item1
+            item4 = item2
 
-def func(initial_text, o):
-    match = re.search('(@[^#]*)#', initial_text[o:])
-    if match:
-        a = match.group(1)
-        r = initial_text[o:].replace(a + '#', a, 1)
-        initial_text = initial_text.replace(initial_text[o:], r, 1)
-    return initial_text
+t = np.array(sets[f'sett{item3}'])
+y = np.array(sets[f'sett{item4}'])
+if np.linalg.det(t) > np.linalg.det(y):
+    v = np.dot(t, y)
+elif np.linalg.det(t) < np.linalg.det(y):
+    v = np.dot(y, t)
+else:
+    v = np.dot(t, y)
 
-initial_text = input()
+g = np.around(np.linalg.inv(v), decimals=3)
+for item in g:
+    print(*item)
 
-e = initial_text.count('@')
-r = 0
-o = 0
-
-while o < len(initial_text):
-    if r < e and initial_text[o] == '@' and bool(re.search('(@[^#]*)#', initial_text)):
-        initial_text = func(initial_text, o)
-        r += 1
-    o += 1
-
-initial_text = initial_text.strip()
-initial_text = re.sub(r'\s+', ' ', initial_text)
-s = re.sub(r'\\n', "\n", initial_text)
-print(f'Formatted Text: {s}')
